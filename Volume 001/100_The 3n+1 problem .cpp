88595dfb1e217b55ff1dab1007c59ceedb059ea9
Monocono <39505929+Monocono@users.noreply.github.com>
2018-11-07 12:00:57 +0900

    Uva Volume 100 - 199

---
+++ Volume 001/100_The 3n+1 problem .cpp
@@ -0,0 +1,42 @@
+/*
+The 3n + 1 problem
+Time limit - 3sec
+Language C++ 11 5.3.0
+Modify 2018 11 07
+*/
+#include <iostream> 
+#define swap(A,B) { int temp; temp = A,A = B , B = temp; }
+
+int Calculation(int A, int B)
+{
+	int temp, cycle, max, i;
+	if (A > B)
+	{
+		swap(A, B);
+	}
+	max = 0;
+	for (i = A; i <= B; i++)
+	{
+		cycle = 1;
+		temp = i;
+		while (temp > 1)
+		{
+			if (temp % 2 == 0)
+				temp /= 2;
+			else
+				temp = (temp * 3) + 1;
+			cycle++;
+		}
+		if (max <= cycle) max = cycle;
+	}
+	return max;
+}
+int main()
+{
+	int A, B;
+	while (scanf("%d %d", &A, &B) != EOF)
+	{
+		printf("%d %d %d\n", A, B, Calculation(A, B));
+	}
+	return 0;
+}
